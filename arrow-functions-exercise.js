f71ba6ac4617ccd7d0df390648b8c76f81b76b07
Saif <saifisami2005@gmail.com>
2023-10-05 00:31:56 -0400

    Finished the exercise.

---
+++ arrow-functions-exercise.js
@@ -0,0 +1,28 @@
+//ES5 MAP CALLBACK
+
+// function double(arr) {
+//     return arr.map(function(val) {
+//       return val * 2;
+//     });
+//   }
+
+//ES2015 VERSION
+const double = arr => arr.map(val => val*2);
+
+
+//Refactor the following function to use arrow functions:
+    //Replace ALL functions with arrow functions
+
+    // function squareAndFindEvens(numbers){
+    //     var squares = numbers.map(function(num){
+    //       return num ** 2;
+    //     });
+    //     var evens = squares.filter(function(square){
+    //       return square % 2 === 0;
+    //     });
+    //     return evens;
+    //   }
+
+//ES2015 VERSION
+
+const squareAndFindEvens = numbers => numbers.map(num => num**2).filter(square => square % 2 === 0);
